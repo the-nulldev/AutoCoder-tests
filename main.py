838187e2be033f797b0061c1e924a56acda23e3e
GitHub Action <action@github.com>
2024-04-01 07:44:58 +0000

    Add code snippets from issue #3

---
+++ main.py
@@ -0,0 +1,11 @@
+```python
+def main():
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    
+    print("Hello, " + name + "! You are " + age + " years old.")
+    print("Goodbye, " + name + "!")
+    
+if __name__ == "__main__":
+    main()
+```
